c7a4b943c4ca8ded4c7fe77268fdecff63294add
mariusToba <mariustoba@protonmail.com>
2019-07-21 21:34:35 +0300

    Completed a tutorial regarding the functional components and hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Todo from './Todo'
+import TodoForm from './TodoForm'
 import './App.css';
 
 function App() {
+
+  const [todos, setTodos] = useState([
+    {
+      text: "Learn about react",
+      isCOmpleted: false
+    },
+    {
+      text: "Meet friend for lunch",
+      isCOmpleted: false
+    },
+    {
+      text: "Build todo app",
+      isCOmpleted: false
+    },
+  ])
+
+  const addTodo = text => {
+    const NewTodos = [...todos, { text }];
+    setTodos(NewTodos);
+  }
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  }
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} completeTodo={completeTodo} removeTodo={removeTodo} />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
     </div>
   );
 }
